2f01e1bfa55d978b3fb04322903ccebf802515bd
namisan <mongtamquoc2015@gmail.com>
2020-11-29 17:13:57 +0700

    Add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mysql = require("mysql");
+var cors = require("cors");
 
 var app = express();
 
@@ -27,6 +28,7 @@ var answersRouter = require("./routes/answers");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 app.use(logger("dev"));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
